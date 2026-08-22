d7d9e85106ef6a823f2f50c6ecbcac222a60ae0b
Lopezzavc <andresitow2@gmail.com>
2026-08-22 16:04:18 -0500

    Wrap Header in Suspense boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/layout/header";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
       className={`dark ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Header />
+        <Suspense>
+          <Header />
+        </Suspense>
         <main className="flex-1">{children}</main>
       </body>
     </html>
